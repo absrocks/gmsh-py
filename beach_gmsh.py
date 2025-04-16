fbcb415b8d8d005d9a7baf6fc059327eda4ff04b
absrocks <am2455@njit.edu>
2025-04-15 21:59:53 -0400

    Added variable size

--- beach_gmsh.py
+++ beach_gmsh.py
@@ -8,7 +8,7 @@ gmsh.model.add("beach_profile")
 # Parameters
 x0, x1 = 0, 70
 h0, h1 = 1.5, 9.0
-n_tau = 2
+n_tau = 1.5
 n_epsilon = 0.75 * (n_tau + 1)
 beta = (n_epsilon - 1) / 2
 exponent = 1 + beta
@@ -20,7 +20,12 @@ h_vals = ((x_vals * alpha) + h0 ** exponent) ** (1 / exponent)
 lc = 1.0
 profile_pts = []
 for i, (x, z) in enumerate(zip(x_vals, h_vals)):
-    gmsh.model.geo.addPoint(x, 0, z, lc, i+1)
+    if x == x1 and z == h1:
+        gmsh.model.geo.addPoint(x1, 0, h1, lc / 4, i+1)
+    elif x == x0 and z == h0:
+        gmsh.model.geo.addPoint(x0, 0, h0, lc / 4, i+1)
+    else:
+        gmsh.model.geo.addPoint(x, 0, z, lc, i+1)
     profile_pts.append(i+1)
 
 # Add bottom rectangle corners
@@ -43,7 +48,6 @@ gmsh.model.geo.addLine(profile_pts[-1], base_right, right_line)
 
 left_line = right_line + 1
 gmsh.model.geo.addLine(base_left, profile_pts[0], left_line)
-
 curve_loop = line_tags + [right_line, bottom_line, left_line]
 gmsh.model.geo.addCurveLoop(curve_loop, 1)
 gmsh.model.geo.addPlaneSurface([1], 1)
