135357cf041894cebac8ae234e0e5011f86900e8
absrocks <am2455@njit.edu>
2025-04-17 01:02:20 -0400

    Added variable size

--- beach_gmsh.py
+++ beach_gmsh.py
@@ -50,17 +50,17 @@ for x, z in zip(x_vals, h_vals):
     point_id += 1
 
 # Flat bottom section (shoreline to left)
-for x in np.arange(x1, x2, lc/4):
+for x in np.arange(x1+lc/4, x2+lc/4, lc/4):
     gmsh.model.geo.addPoint(x, 0, h_max, lc / 4, point_id)
     profile_pts.append(point_id)
     point_id += 1
 
 # Base rectangle points (not added to profile_pts!)
-gmsh.model.geo.addPoint(x0, 0, 0, lc, point_id)
+gmsh.model.geo.addPoint(x0, 0, -2, lc, point_id)
 base_left = point_id
 point_id += 1
 
-gmsh.model.geo.addPoint(x2, 0, 0, lc, point_id)
+gmsh.model.geo.addPoint(x2, 0,-2, lc, point_id)
 base_right = point_id
 point_id += 1
 
@@ -75,21 +75,27 @@ top_line = len(line_tags) + 1
 gmsh.model.geo.addLine(base_right, base_left, top_line)
 
 right_line = top_line + 1
-gmsh.model.geo.addLine(base_right, profile_pts[-1], right_line)
+gmsh.model.geo.addLine(profile_pts[-1], base_right, right_line)
 
 left_line = right_line + 1
-#print(profile_pts[-1], base_left, left_line)
-gmsh.model.geo.addLine(profile_pts[0], base_left, left_line)
-curve_loop = line_tags + [left_line, top_line, right_line]
-#gmsh.model.geo.addCurveLoop(curve_loop, 1)
-#gmsh.model.geo.addPlaneSurface([1], 1)
 
-gmsh.option.setNumber("Geometry.PointNumbers", 1)
+gmsh.model.geo.addLine(base_left, profile_pts[0], left_line)
+curve_loop = line_tags + [left_line, top_line, right_line]
+gmsh.model.geo.addCurveLoop(curve_loop, 1)
+gmsh.model.geo.addPlaneSurface([1], 1)
+
+#gmsh.option.setNumber("Geometry.PointNumbers", 1)
 #gmsh.option.setNumber("Geometry.LineNumbers", 1)
 gmsh.model.geo.synchronize()
 gmsh.model.addPhysicalGroup(2, [1], 1)
+
+h = 0.4
+ov = gmsh.model.geo.extrude([(2, 1)], 0, 0, h, [2, 2], [0.5, 1])
+gmsh.model.geo.synchronize()
+gmsh.model.addPhysicalGroup(3, [1, 2, ov[1][1]], 1001)
+gmsh.model.mesh.generate(3)
 #gmsh.model.mesh.generate(2)
-gmsh.write("beach_profile.msh")
+gmsh.write("beach_3d.msh")
 
 
 if "-nopopup" not in sys.argv:
